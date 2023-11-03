be40e925ca6994fc55031e8e143460cc899f5b07
Ifeanyi Ukadike <ffrank539@gmail.com>
2023-11-03 02:14:53 +0100

    feat: color flipper
    The background of the page changes when a button is clicked

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,12 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(150,75,0)', '#FFFF00'];
+
+const btn = document.querySelector('#btn');
+const bgcolor = document.querySelector('#bgcolor');
+
+// add event listener
+btn.addEventListener('click', function () {
+  const body = document.querySelector('body');
+  let colorSelector = Math.floor(Math.random() * 10) % colors.length;
+  body.style.backgroundColor = colors[colorSelector];
+  bgcolor.textContent = colors[colorSelector];
+});
--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,1 +1,19 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const btn = document.querySelector('#btn');
+const bgcolor = document.querySelector('#bgcolor');
+
+// trying out arrow function ;)
+let hexSelector = (hexIndex) => hex[hexIndex];
+
+// add event listener
+btn.addEventListener('click', function () {
+  const body = document.querySelector('body');
+  let colorSelector = '#';
+  for (let i = 0; i < 6; i++) {
+    let hexIndex = Math.floor(Math.random() * 100) % hex.length;
+    colorSelector += hexSelector(hexIndex);
+  }
+  body.style.backgroundColor = colorSelector;
+  bgcolor.textContent = colorSelector;
+});
