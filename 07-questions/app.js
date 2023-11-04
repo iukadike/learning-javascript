b93c8c9f5e5628e9c9be1d6d5fbe3aa3ceffb99a
Ifeanyi Ukadike <ffrank539@gmail.com>
2023-11-04 23:11:06 +0100

    feat: questions
    This project involves practicing the following:
    1. using selectors inside the element
    2. traversing the DOM

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,2 +1,34 @@
-//using selectors inside the element
 // traversing the dom
+/*
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach((btn) =>
+  btn.addEventListener('click', () => {
+    question = btn.parentElement.parentElement;
+    question.classList.toggle('show-text');
+  })
+);
+ */
+
+/***************************************
+ *  using selectors inside the element  *
+ ***************************************/
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+  btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', () => {
+    checkCurrent(question);
+    question.classList.toggle('show-text');
+  });
+});
+
+// making sure that at most one item is expanded
+checkCurrent = (selected) => {
+  questions.forEach((item) => {
+    if (item !== selected) {
+      item.classList.remove('show-text');
+    }
+  });
+};
