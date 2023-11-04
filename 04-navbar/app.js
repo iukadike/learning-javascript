f331bba455a64c0bf3682a1f71bcdf0d168999c7
Ifeanyi Ukadike <ffrank539@gmail.com>
2023-11-04 11:09:45 +0100

    feat: navbar
    This project displays or hides the navigation links using javascript on
    mobile devices when the nav toggle is clicked.

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -1,5 +1,12 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+// set variable for links and toggle-bar
+const links = document.querySelector('.links');
+const toggleBar = document.querySelector('.nav-toggle');
+
+// set an event listerner for when nav-toggle is clicked
+// toggleBar.addEventListener('click', () => {
+//   links.classList.contains('show-links')
+//     ? links.classList.remove('show-links')
+//     : links.classList.add('show-links');
+
+// set an event listerner for when nav-toggle is clicked - revised
+toggleBar.addEventListener('click', () => links.classList.toggle('show-links'));
